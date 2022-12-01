1f8d01cab5ce65364b0f3946b4a2c0705e32db02
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2022-12-01 12:48:29 +0700

    feat: add authentication page including register and login forms

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,7 +1,31 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import AuthForm from "../components/AuthForm";
+
 export default function AuthPage() {
+  const { palette } = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <div>
-      <h1>AuthPage</h1>
-    </div>
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          IPB Kehilangan
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "90%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={palette.background.alt}
+      >
+        <AuthForm />
+      </Box>
+    </Box>
   );
 }
